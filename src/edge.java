109e80cd4f018d2153e50f68e3700e4234e4ddf2
zhnuraa <250822@astanait.edu.kz>
2026-05-10 12:23:49 +0500

    Update edge.java

--- src/edge.java
+++ src/edge.java
@@ -1,1 +1,12 @@
-
+public class Edge {
+    private final int source;
+    private final int destination;
+    public Edge(int source, int destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+    public int getSource() { return source; }
+    public int getDestination() { return destination; }
+    @Override
+    public String toString() { return source + "->" + destination; }
+}
