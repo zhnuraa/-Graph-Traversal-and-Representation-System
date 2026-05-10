e944c6d6c243f0bb34145fce60b50bf6b7678517
zhnuraa <250822@astanait.edu.kz>
2026-05-10 12:23:37 +0500

    Update vertex.java

--- src/vertex.java
+++ src/vertex.java
@@ -1,1 +1,7 @@
-
+public class Vertex {
+    private final int id;
+    public Vertex(int id) { this.id = id; }
+    public int getId() { return id; }
+    @Override
+    public String toString() { return "Vertex{" + id + "}"; }
+}
