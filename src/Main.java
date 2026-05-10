24dd0d13360b86168be0af01c1071cd91639f447
zhnuraa <250822@astanait.edu.kz>
2026-05-10 12:24:42 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,7 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Graph Traversal Performance Experiment");
+        System.out.println("=====================================");
+        Experiment.runMultipleTests();
+    }
+}
