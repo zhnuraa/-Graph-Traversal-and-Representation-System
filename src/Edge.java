54848e2fd099b8c4f9655b6b02747cdad37d8e87
zhnuraa <250822@astanait.edu.kz>
2026-05-23 10:41:16 +0500

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,22 +1,20 @@
 public class Edge {
     private final int source;
     private final int destination;
+    private final int weight;          // new field
 
-    public Edge(int source, int destination) {
+    public Edge(int source, int destination, int weight) {
         this.source = source;
         this.destination = destination;
+        this.weight = weight;
     }
 
-    public int getSource() {
-        return source;
-    }
-
-    public int getDestination() {
-        return destination;
-    }
+    public int getSource() { return source; }
+    public int getDestination() { return destination; }
+    public int getWeight() { return weight; }
 
     @Override
     public String toString() {
-        return source + " -> " + destination;
+        return source + " -> " + destination + " (weight: " + weight + ")";
     }
 }
